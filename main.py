4d28b3ce1d5bb848ed6e2c8bb1dd3d085f59880f
jeng413 <estevansix13@gmail.com>
2023-06-02 11:43:02 -0500

    prueba

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print 
+  if intentos == 0:
+        golosa[2] = " |       O "
+    elif intentos == 1:
+        golosa[1] = " |       | "
+    elif intentos == 2:
+        golosa[1] = " |      /| "
+    elif intentos == 3:
+        golosa[1] = " |      /|\\"
+    elif intentos == 4:
+        golosa[3] = " \\       | "
+    elif intentos == 5:
+        golosa[3] = " \\      /| "
+    elif intentos == 6:
+        golosa[3] = " \\      /|\\"
