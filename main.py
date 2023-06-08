96681b0ff56e5a7577cc12e38fe245b8db53ed83
Miguel puerta <miguelpuerta331110@correo.itm.edu.co>
2023-06-08 10:38:54 -0500

    Completo

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-<<<<<<< HEAD
- for linea in golosa:
+import random
+
+def dibujar_golosa(intentos):
+    golosa = [
+        "   -----   ",
+        " /       \\ ",
+        " |       | ",
+        " \\       / ",
+        "   -----   "
+    ]
+    
+    if intentos == 0:
+        golosa[2] = " |       O "
+    elif intentos == 1:
+        golosa[3] = " |       | "
+    elif intentos == 2:
+        golosa[3] = " |      /| "
+    elif intentos == 3:
+        golosa[3] = " |      /|\\"
+    elif intentos == 4:
+        golosa[4] = " \\       | "
+    elif intentos == 5:
+        golosa[4] = " \\      /  "
+    elif intentos == 6:
+        golosa[4] = " \\      / \\"
+    
+    for linea in golosa:
         print(linea)
 
 def dibujar_cara_feliz():
@@ -27,46 +52,6 @@ def dibujar_calavera():
     for linea in calavera:
         print(linea)
 
-=======
-<<<<<<< HEAD
-  if intentos == 0:
-=======
- import random
-
-def dibujar_golosa(intentos):
-    golosa = [
-        "   -----   ",
-        " /       \\ ",
-        " |       |  ",
-        " \\       / ",
-        "   -----    ",
-    ]
-    
-    if intentos == 0:
->>>>>>> d9414bf8b498fd3e3b96fe08b76fcb4a07d827b3
-        golosa[2] = " |       O "
-    elif intentos == 1:
-        golosa[1] = " |       | "
-    elif intentos == 2:
-        golosa[1] = " |      /| "
-    elif intentos == 3:
-        golosa[1] = " |      /|\\"
-    elif intentos == 4:
-        golosa[3] = " \\       | "
-    elif intentos == 5:
-        golosa[3] = " \\      /| "
-    elif intentos == 6:
-<<<<<<< HEAD
-        golosa[3] = " \\      /|\\"
-=======
-        golosa[3] = " \\      /|\\"
-    
-    for linea in golosa:
-        print(linea)
-
-
-
->>>>>>> 24006483e6e75e486696aa892c1b81a03ca6547f
 def jugar_golosa():
     print("¡Bienvenido a La Golosa!")
     print("¡Gira la ruleta para obtener tu número de vidas!")
@@ -83,3 +68,44 @@ def jugar_golosa():
     
     objetivo = random.randint(1, 100)
     intentos = 0
+    
+    print("Estoy pensando en un número entre 1 y 100.")
+    print("Tu objetivo es adivinarlo en la menor cantidad de intentos.")
+    
+    while True:
+        print()
+        dibujar_golosa(intentos)
+        
+        intento = input("Ingresa tu número ('H' para obtener una pista): ")
+        
+        if intento.upper() == "H":
+            pista = "El número objetivo es aproximadamente {}".format(objetivo)
+            print(pista)
+            continue
+        
+        try:
+            intento = int(intento)
+        except ValueError:
+            print("Entrada inválida. Por favor, ingresa un número válido.")
+            continue
+        
+        intentos += 1
+        
+        if intento < objetivo:
+            print("El número que estás buscando es más grande.")
+        elif intento > objetivo:
+            print("El número que estás buscando es más pequeño.")
+        else:
+            print("¡Felicidades! ¡Has adivinado el número en {} intentos!".format(intentos))
+            dibujar_cara_feliz()
+            break
+        
+        if intentos == num_vidas:
+            print()
+            dibujar_golosa(intentos)
+            print("¡Lo siento! Has agotado tus intentos.")
+            print("El número que estaba buscando era: {}".format(objetivo))
+            dibujar_calavera()
+            break
+
+jugar_golosa()
